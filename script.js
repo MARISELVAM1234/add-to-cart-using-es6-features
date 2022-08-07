ed3e6ccfd42cea289ca0fd20bfcc8b990a2374b1
selvam <selvamplkm111@gmail.com>
2022-08-07 22:22:04 +0530

    update.script.file

--- script.js
+++ script.js
@@ -63,50 +63,31 @@ const aircoolers = [
 ];
 
 //addto cart-part
-let ab;
 let arr = [];
-let c;
 var valElement = document.getElementById("val");
 let addBtn = document.querySelector("#add-btn");
 addBtn.addEventListener("click", () => {
   let inputValue = parseInt(valElement.value);
-
-  if (inputValue < 10) {
-    console.log("Input value : ", inputValue);
-    c = inputValue - 1;
-    //   console.log(c,"c");
-    // console.log(inputValue,'value');
-    var b = aircoolers[c].id;
-    // console.log(b,'index');
-    // if (inputValue == b) {
-    ab = aircoolers[c];
-    if (!arr.includes(ab)) {
-      arr.push(ab);
+  aircoolers.map((element) => {
+    if (element.id === inputValue && !arr.includes(element)) {
+      console.log("Input value : ", inputValue);
+      arr.push(element);
       valElement.value = "";
     }
-    // }
-  }
+  });
 });
 
 //remove-btn part
 var removeElement = document.querySelector("#remove-btn");
 removeElement.addEventListener("click", () => {
-  let inputValue = parseInt(valElement.value);
-  console.log(inputValue);
-  if (inputValue < 10) {
-  
-    var removeEle = arr.findIndex((arrValue) => {
-      // console.log(arrValue);
-   return arrValue.id == inputValue;
-      // console.log(data);
-    });
-   //  if(arr.includes(removeEle)){
-    console.log(removeEle);
-    arr.splice(removeEle, 1);
-    console.log(arr);
-    valElement.value = "";
-   //  }
-  }
+  let inputValue = parseInt(valElement.value );
+  console.log(inputValue );
+var remove = (ele)=>{
+  // var ab  = arr[ac]
+return ele.id < inputValue
+}
+var filterEle =arr.filter(remove)
+console.log(filterEle);
 });
 
 //view-btn-part
